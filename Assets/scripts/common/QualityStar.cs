95086e00a1f875e94593ed87f7fba0756b50a437
kanontang@gmail.com <kanontang@gmail.com>
2015-05-08 18:07:41 +0800

    增加一个品质星星的功能。

---
+++ Assets/scripts/common/QualityStar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+public class QualityStar
+{
+
+    public static GameObject create(String path, int num, float gap)
+    {
+        GameObject qs = new GameObject();
+        qs.name = "qualityStar";
+        for (int i = 0; i < num; ++i)
+        {
+            GameObject star = new GameObject();
+            star.name = "star" + i;
+            Image image = star.AddComponent<Image>();
+            //添加图片资源
+            image.overrideSprite = Resources.Load(path, typeof(Sprite)) as Sprite;
+            star.transform.parent = qs.transform;
+        }
+        return qs;
+    }
+
+}
---
+++ Assets/scripts/test/QualityStarTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QualityStarTest : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        QualityStar.create("star", 3, 1);
+        QualityStar.create("star", 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
