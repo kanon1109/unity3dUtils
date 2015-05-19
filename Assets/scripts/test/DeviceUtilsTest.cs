0e528b723287962439019fdcc5772cc96a382f28
kanontang@gmail.com <kanontang@gmail.com>
2015-05-19 17:44:46 +0800

    增加一个设备号工具。

---
+++ Assets/scripts/test/DeviceUtilsTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DeviceUtilsTest : MonoBehaviour 
+{
+    public GameObject infoTxt;
+	// Use this for initialization
+	void Start () 
+    {
+        infoTxt.GetComponent<Text>().text = "info:\n";
+        infoTxt.GetComponent<Text>().text += "GetDeviceID: " + DeviceUtils.GetDeviceID() + "\n";
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
