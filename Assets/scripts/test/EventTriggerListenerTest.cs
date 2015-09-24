22d9cf67dd820e27c548069032149a3bc9f12607
kanontang@gmail.com <kanontang@gmail.com>
2015-09-24 11:27:27 +0800

    增加交互事件触发器。

---
+++ Assets/scripts/test/EventTriggerListenerTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EventTriggerListenerTest : MonoBehaviour
+{
+    public GameObject btn;
+    void Start()
+    {
+        EventTriggerListener.Get(btn).onDown = onDownHandler;
+    }
+
+    private void onDownHandler(GameObject go)
+    {
+        print("onDownHandler");
+    }
+}
---
+++ Assets/scripts/utils/EventTriggerListener.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// 事件触发封装给gameObject添加交互事件 - 需要什么事件可扩展 
+/// Event trigger listener.
+/// </summary>
+public class EventTriggerListener : MonoBehaviour,  
+                                    IPointerClickHandler,  
+                                    IPointerDownHandler,  
+                                    IPointerEnterHandler,  
+                                    IPointerExitHandler,  
+                                    IPointerUpHandler,  
+                                    ISelectHandler,  
+                                    IUpdateSelectedHandler,  
+                                    IDeselectHandler,
+                                    IDragHandler,
+                                    IEndDragHandler,
+                                    IDropHandler,
+                                    IScrollHandler,
+                                    IMoveHandler
+{
+    public delegate void VoidDelegate (GameObject go);
+    public VoidDelegate onClick;
+    public VoidDelegate onDown;
+    public VoidDelegate onEnter;
+    public VoidDelegate onExit;
+    public VoidDelegate onUp;
+    public VoidDelegate onSelect;
+    public VoidDelegate onUpdateSelected;
+    public VoidDelegate onDeselect;
+    public VoidDelegate onDrag;
+    public VoidDelegate onEndDrag;
+    public VoidDelegate onDrop;
+    public VoidDelegate onScroll;
+    public VoidDelegate onMove;
+
+    static public EventTriggerListener Get (GameObject go)
+    {
+        EventTriggerListener listener = go.GetComponent<EventTriggerListener>();
+        if (listener == null) listener = go.AddComponent<EventTriggerListener>();
+        return listener;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(onClick != null) onClick(gameObject);
+    }
+
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        if(onDown != null) onDown(gameObject);
+    }
+
+    public void OnPointerEnter (PointerEventData eventData)
+    {
+        if(onEnter != null) onEnter(gameObject);
+    }
+
+    public void OnPointerExit (PointerEventData eventData)
+    {
+        if(onExit != null) onExit(gameObject);
+    }
+
+    public void OnPointerUp (PointerEventData eventData)
+    {
+        if(onUp != null) onUp(gameObject);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        if (onSelect != null) onSelect(gameObject);
+    }
+
+    public void OnUpdateSelected(BaseEventData eventData)
+    {
+        if (onUpdateSelected != null) onUpdateSelected(gameObject);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        if (onDeselect != null) onDeselect(gameObject);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (onDrag != null) onDrag(gameObject);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (onEndDrag != null) onEndDrag(gameObject);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (onDrop != null) onDrop(gameObject);
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        if (onScroll != null) onScroll(gameObject);
+    }
+
+    public void OnMove(AxisEventData eventData)
+    {
+        if (onMove != null) onMove(gameObject);
+    }
+}
