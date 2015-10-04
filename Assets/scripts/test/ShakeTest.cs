2326985bc7092da1cc6c0e34e6e7d00413ba7f36
kanontang <kanontang@gmail.com>
2015-10-04 22:02:29 +0800

    增加震动功能。

---
+++ Assets/scripts/test/ShakeTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class ShakeTest : MonoBehaviour
+{
+    private GameObject camera;
+    private Shake shake;
+    public Button button;
+
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera");
+        shake = this.GetComponent<Shake>();
+        button.onClick.AddListener(clickHandler);
+    }
+
+    private void clickHandler()
+    {
+        shake.shake(this.gameObject);
+    }
+
+}
