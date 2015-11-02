df033bae1e71d23a89627ce152c3047ee1de11fc
kanontang@gmail.com <kanontang@gmail.com>
2015-11-02 18:11:38 +0800

    增加UI变灰功能。

---
+++ Assets/scripts/test/UiUtilTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UiUtilTest : MonoBehaviour
+{
+    public Button btn;
+    public Text txt;
+    public Image img;
+
+    public GameObject btnGo;
+    public GameObject txtGo;
+    public GameObject imgGo;
+
+    void Start()
+    {
+        UiUtil.setBtnGray(btn, false);
+        UiUtil.setUIGray(txt.gameObject, true);
+        UiUtil.setUIGray(img.gameObject, true);
+
+    }
+}
---
+++ Assets/scripts/utils/UiUtil.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UiUtil : MonoBehaviour
+{
+    /// <summary>
+    /// 设置按钮变灰色
+    /// </summary>
+    /// <returns>直接返回传入参数</returns>
+	public static bool setBtnGray(Button btn, bool bl)
+    {
+        btn.interactable = bl;
+        Material m = null;
+        if ( !bl ) m = Resources.Load("Material/UIGray") as Material;
+        btn.GetComponent<Image>().material = m;
+        btn.GetComponentInChildren<Text>().material = m;
+        return bl;
+    }
+
+    /// <summary>
+    /// 变灰Ui对象(Image或Text)
+    /// </summary>
+    /// <param name="Go"></param>
+    public static void setUIGray(GameObject obGo, bool bl)
+    {
+        Material m = null;
+        if (bl) m = Resources.Load("Material/UIGray") as Material;
+        Image img = obGo.GetComponent<Image>();
+        if (img != null)
+        {
+            img.material = m;
+        }
+        else
+        {
+            Text tf = obGo.GetComponent<Text>();
+            if (tf != null)
+                tf.material = m;
+        }
+    }
+}
