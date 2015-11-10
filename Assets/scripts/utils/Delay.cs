b08d3fca6242bbef88f6374ab76e136820bb8f30
kanontang@gmail.com <kanontang@gmail.com>
2015-11-10 19:44:56 +0800

    简化间隔函数方法。

--- Assets/scripts/utils/Delay.cs
+++ Assets/scripts/utils/Delay.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+//延迟工具
 public class Delay : MonoBehaviour
 {
     //回调
@@ -6,30 +7,29 @@ public class Delay : MonoBehaviour
     private static DelayCompleteHandler delayCompleteHandler = null;
     private static float millisecond = 0;
     private static bool isComplete = false;
-    /// <summary>
-    /// 创建
-    /// </summary>
-    /// <param name="go">需要绑定此脚本的对象</param>
-    /// <returns></returns>
-    public static void create(GameObject go)
-    {
-        if (go == null) return;
-        Delay delay = go.GetComponent<Delay>();
-        if (delay == null)
-            delay = go.AddComponent<Delay>();
-    }
+    //是否自动销毁
+    private static bool autoDestroy = false;
+    private static Delay delay = null;
 
     /// <summary>
     /// 设置间隔
     /// </summary>
+    /// <param name="targetGo">需要绑定此脚本的对象</param>
     /// <param name="millisecond">毫秒数</param>
     /// <param name="delayCompleteHandler">回调</param>
+    /// <param name="autoDestroy">是否在结束时销毁脚本</param>
     /// <returns></returns>
-    public static void setDelay(float millisecond, DelayCompleteHandler delayCompleteHandler)
+    public static void setDelay(GameObject targetGo, float millisecond, 
+                                DelayCompleteHandler delayCompleteHandler, 
+                                bool autoDestroy = true)
     {
+        if (targetGo == null) return;
+        Delay.delay = targetGo.GetComponent<Delay>();
+        if (Delay.delay == null) Delay.delay = targetGo.AddComponent<Delay>();
         Delay.isComplete = false;
         Delay.millisecond = millisecond;
         Delay.delayCompleteHandler = delayCompleteHandler;
+        Delay.autoDestroy = autoDestroy;
     }
 
     void FixedUpdate()
@@ -42,6 +42,12 @@ public class Delay : MonoBehaviour
             Delay.millisecond = 0;
             if (Delay.delayCompleteHandler != null)
                 Delay.delayCompleteHandler.Invoke();
+            if (Delay.autoDestroy)
+            {
+                //销毁
+                GameObject.Destroy(Delay.delay);
+                Delay.delay = null;
+            }
         }
     }
 }
