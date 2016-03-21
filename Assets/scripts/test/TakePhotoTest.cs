61223908976e5c7bdba51b363e3ca5ed97f13cdd
kanontang@gmail.com <kanontang@gmail.com>
2016-03-21 14:26:24 +0800

    增加截图并保存功能。

---
+++ Assets/scripts/test/TakePhotoTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+public class TakePhotoTest : MonoBehaviour
+{
+    public Button btn;
+    public void Start()
+    {
+        btn.onClick.AddListener(btnClickHandler);
+    }
+
+    private void btnClickHandler()
+    {
+        StartCoroutine(TakePhoto.takeAndSave("test.png"));
+    }
+}
---
+++ Assets/scripts/utils/TakePhoto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+/// <summary>
+/// 截图保存
+/// </summary>
+public class TakePhoto
+{
+    public static IEnumerator takeAndSave(string name)
+    {
+        //一定先要调用这个
+        yield return new WaitForEndOfFrame();
+        Texture2D photoTex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        photoTex.ReadPixels(new Rect(0.0f, 0.0f, Screen.width * 1.0f, Screen.height * 1.0f), 0, 0);
+        photoTex.Apply();
+        //将图片转成2进制流
+        byte[] bytes = photoTex.EncodeToPNG();
+        //保持至本地
+        FileManager.saveByte(name, bytes);
+    }
+}
