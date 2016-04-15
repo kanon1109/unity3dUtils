ee0e03656026d07bd65c3481882f60abbcb16222
kanontang <kanontang@gmail.com>
2016-04-15 12:14:56 +0800

    增加点击移动功能，

---
+++ Assets/Scripts/common/TouchedMoveFollow.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+/// <summary>
+/// 点击移动跟随
+/// </summary>
+public class TouchedMoveFollow:MonoBehaviour
+{
+    //是否初始化
+    private bool isInt = false;
+    //鼠标按下
+    private bool isMouseDown = false;
+    //鼠标位置x坐标
+    private float mouseX;
+    //鼠标位置y坐标
+    private float mouseY;
+    //需要跟随的对象
+    public GameObject targetGo;
+    //缓动系数
+    public float ease = .04f;
+    //是否作用到target z坐标上
+    public bool usePosZ = true;
+    /// <summary>
+    /// 更新鼠标（点击后拖动）控制
+    /// </summary>
+    private void updateMouseControl()
+    {
+        if (!this.isInt)
+        {
+            this.mouseX = Input.mousePosition.x;
+            this.mouseY = Input.mousePosition.y;
+            this.isInt = true;
+        }
+        if (Input.GetMouseButtonDown(0)) this.isMouseDown = true;
+        if (Input.GetMouseButtonUp(0)) this.isMouseDown = false;
+
+        if (this.isMouseDown)
+        {
+            this.mouseX = Input.mousePosition.x;
+            this.mouseY = Input.mousePosition.y;
+        }
+
+        if (this.targetGo != null)
+        {
+            Vector3 mousePos = new Vector3(this.mouseX,
+                                           this.mouseY,
+                                           this.targetGo.transform.localPosition.z); //必须把z坐标带入才能转化世界坐标
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            //缓动鼠标跟随公式
+            float vx = (mousePos.x - this.targetGo.transform.localPosition.x) * this.ease;
+            float vy = (mousePos.y - this.targetGo.transform.localPosition.y) * this.ease;
+            float vz = (mousePos.y - this.targetGo.transform.localPosition.y) * this.ease;
+            if (this.usePosZ) vy = 0;
+            else vz = 0;
+            this.targetGo.transform.localPosition = new Vector3(this.targetGo.transform.localPosition.x + vx,
+                                                                this.targetGo.transform.localPosition.y + vy,
+                                                                this.targetGo.transform.localPosition.z + vz);
+        }
+    }
+
+    void Update()
+    {
+        this.updateMouseControl();
+    }
+}
---
+++ Assets/Scripts/test/ColorUtilTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class ColorUtilTest:MonoBehaviour
+{
+    public Image img;
+    void Start()
+    {
+        img.material.SetColor("_Color", ColorUtil.uint2Color(ColorUtil.GOLD));
+        Debug.Log(ColorUtil.uint2Color(ColorUtil.GOLD));
+    }
+}
