067dedbfe82f2030bfcec7fd4fdcd0c80828af55
kanontang <kanontang@gmail.com>
2016-05-06 16:20:06 +0800

    增加一个2dui 跟随 3d物体的脚本。

---
+++ Assets/Scripts/common/UIFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 2d ui跟随3d 对象（例血条跟随角色） 
+/// </summary>
+public class UIFollow : MonoBehaviour
+{
+    //跟随的目标
+    public Transform targetTransform;
+    //ui
+    public Transform uiTransform;
+    //偏移
+    public Vector2 offset = Vector2.zero;
+    //目标与摄像机的前后距离
+    private float disZ;
+    // Use this for initialization
+    void Start()
+    {
+        if (this.targetTransform == null || this.uiTransform == null) return;
+        this.disZ = Mathf.Abs(this.targetTransform.position.z - Camera.main.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.targetTransform == null || this.uiTransform == null) return;
+        float newDisZ = this.disZ / Mathf.Abs(this.targetTransform.position.z - Camera.main.transform.position.z);
+        Vector2 newOffset = new Vector2(this.offset.x * newDisZ, this.offset.y * newDisZ);
+        this.uiTransform.localScale = Vector3.one * newDisZ;
+        Vector3 newPos = Camera.main.WorldToScreenPoint(this.targetTransform.position);
+        this.uiTransform.position = new Vector3(newPos.x + newOffset.x, newPos.y + newOffset.y, 0f);
+    }
+}
---
+++ Assets/Scripts/test/HpBarFollowTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HpBarFollowTest : MonoBehaviour
+{
+    public ProgressBar hpBar;
+    public GameObject role;
+    private UIFollow uiFollow;
+    // Use this for initialization
+    void Start()
+    {
+        hpBar.setValue(1);
+        this.uiFollow = this.gameObject.GetComponent<UIFollow>();
+        if (this.uiFollow == null)
+            this.uiFollow = this.gameObject.AddComponent<UIFollow>();
+        this.uiFollow.targetTransform = this.role.transform;
+        this.uiFollow.uiTransform = this.hpBar.transform;
+        this.uiFollow.offset = new Vector2(-this.hpBar.GetComponent<RectTransform>().sizeDelta.x / 2, 40);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //测试代码 移动角色
+        if (Input.GetKey(KeyCode.W))
+            role.transform.Translate(Vector3.forward);
+        if (Input.GetKey(KeyCode.S))
+            role.transform.Translate(Vector3.back);
+        if (Input.GetKeyDown(KeyCode.A))
+            role.transform.Translate(Vector3.left);
+        if (Input.GetKeyDown(KeyCode.D))
+            role.transform.Translate(Vector3.right);
+    }
+}
