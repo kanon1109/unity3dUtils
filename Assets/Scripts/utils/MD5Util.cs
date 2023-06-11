0c49d172d0276d10e86d80b39e81841244e8475c
kanontang <kanontang@gmail.com>
2023-06-11 19:16:17 +0800

    修改md5方法

--- Assets/Scripts/utils/MD5Util.cs
+++ Assets/Scripts/utils/MD5Util.cs
@@ -8,14 +8,12 @@ public class MD5Util
     public static string hash(string input)
     {
         if (md5 == null) md5 = MD5.Create(); 
-        byte[] inputBytes = Encoding.ASCII.GetBytes(input); 
-        byte[] hash = md5.ComputeHash(inputBytes);
-        StringBuilder sb = new StringBuilder();
-        string str = "";
-        for (int i = 0; i < hash.Length; ++i)
-        {
-            str += Convert.ToString(hash[i], 16);
+        byte[] sor = Encoding.UTF8.GetBytes(input);
+        byte[] result = md5.ComputeHash(sor);
+        StringBuilder strbul = new StringBuilder(40);
+        for (int i = 0; i < result.Length; i++) {
+            strbul.Append(result[i].ToString("x2"));//加密结果"x2"结果为32位,"x3"结果为48位,"x4"结果为64位
         }
-        return str; 
+        return strbul.ToString();
     }
 }
